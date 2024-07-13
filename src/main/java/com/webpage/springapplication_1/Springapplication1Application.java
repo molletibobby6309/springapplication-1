d072750f0ed7a9899574167cddfe89b91cadbb81
moletibobby6309 <bobbymolleti881@gmail.com>
2024-07-13 10:50:28 +0530

    first commit

---
+++ src/main/java/com/webpage/springapplication_1/Springapplication1Application.java
@@ -0,0 +1,13 @@
+package com.webpage.springapplication_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springapplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springapplication1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/webpage/springapplication_1/Springapplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webpage.springapplication_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springapplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
